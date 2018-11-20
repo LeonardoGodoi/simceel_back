c7b98d0c7e4fe7868d01513c4f999cb0f9d7aebd
Leonardo Godoi <Leogodoi@LeogodoiNote.Leogodoinote>
2018-11-19 22:03:57 -0200

    simceel_back_v02

---
+++ src/test/java/com/simceel/simceelback/SimceelBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simceel.simceelback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimceelBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
